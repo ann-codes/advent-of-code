d73a0a481e411b27178fb98d93f7b3f2e1de2895
Ann Nguyen <annfcz@gmail.com>
2019-12-02 02:00:21 +0000

    completed day1

---
+++ 2019/day01.js
@@ -0,0 +1,25 @@
+const fs = require ("fs")
+let data = fs.readFileSync("day01input.txt", "utf-8").split("\n");
+
+// part1
+const fuelReq = (input) => {
+  let fuel = input.map(mass => Math.floor(Number(mass)/3) - 2)
+  return fuel.reduce((sum, current) => sum + current)
+}
+
+const fuelReq_golf = i => i.map(m => Math.floor(Number(m)/3)-2).reduce((t,cv) => t+cv)
+
+//part2
+const realFuelReq = (input) => {
+  let fuel = input.reduce((totalMass, currentMass) => {
+    currentMass = Math.floor(Number(currentMass)/3)-2
+    while (currentMass > 0) {
+      totalMass.push(currentMass)
+      currentMass = Math.floor(Number(currentMass)/3)-2
+    }
+    return totalMass
+  }, [])
+  return fuel.reduce((sum, current) => sum + current)
+}
+
+console.log(realFuelReq(data))
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ I will code the solutions in a readable format, but may also include some code g
 - [AoC Subreddit](https://www.reddit.com/r/adventofcode/)
 - [Awesome AoC Resource Collection](https://github.com/Bogdanp/awesome-advent-of-code)
 - [ZtM Community AoC](https://github.com/zero-to-mastery/coding_challenge-24)
-- [Ann's Code in Repl.it](https://repl.it/@anncancode/advent-of-code-play)
+- [Ann's Code in Repl.it](https://repl.it/@anncancode/advent-of-code)
 
 ## Problems and Solutions
-- [2019](https://github.com/ann-codes/advent-of-code-play/blob/master/2019/)
+- [2019](https://github.com/ann-codes/advent-of-code/blob/master/2019/)
